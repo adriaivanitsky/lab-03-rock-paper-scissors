97e1d2e9abe16647a7b8ed8239eb7dc4f4797ba0
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-10-06 19:14:41 -0700

    made 9 tests

--- test/example.test.js
+++ test/example.test.js
@@ -1,27 +1,57 @@
-// IMPORT MODULES under test here:
 import { didUserWin } from "../utils.js";
-
-// import { example } from '../example.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true;
-
-    //Expect
-    // Make assertions about what is expected versus the actual result
+test('user: rock computer: rock', (expect) => {
+    const expected = 'tied';
+    const actual = didUserWin('rock', 'rock');
     expect.equal(actual, expected);
 });
 
+test('user: rock computer: paper', (expect) => {
+    const expected = 'lose';
+    const actual = didUserWin('rock', 'paper');
+    expect.equal(actual, expected);
+});
 
-// test('user: rock throw: scissors', (expect) => {
-//     const expected = true;
-//     const actual = 
-// })
+test('user: rock computer: scissors', (expect) => {
+    const expected = 'win';
+    const actual = didUserWin('rock', 'scissors');
+    expect.equal(actual, expected);
+});
+
+test('user: paper computer: rock', (expect) => {
+    const expected = 'lose';
+    const actual = didUserWin('paper', 'rock');
+    expect.equal(actual, expected);
+});
+
+test('user: paper computer: paper', (expect) => {
+    const expected = 'tied';
+    const actual = didUserWin('paper', 'paper');
+    expect.equal(actual, expected);
+});
+
+test('user: paper computer: scissors', (expect) => {
+    const expected = 'lose';
+    const actual = didUserWin('paper', 'scissors');
+    expect.equal(actual, expected);
+});
+
+test('user: scissors computer: rock', (expect) => {
+    const expected = 'lose';
+    const actual = didUserWin('scissors', 'rock');
+    expect.equal(actual, expected);
+});
+
+test('user: scissors computer: paper', (expect) => {
+    const expected = 'win';
+    const actual = didUserWin('scissors', 'paper');
+    expect.equal(actual, expected);
+});
+test('user: scissors computer: scissors', (expect) => {
+    const expected = 'tied';
+    const actual = didUserWin('scissors', 'scissors');
+    expect.equal(actual, expected);
+});
 
