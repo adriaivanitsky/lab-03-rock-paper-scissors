b10578ee70833a08d09e94dce667e9a9debbc205
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-10-07 12:07:20 -0700

    fixed eslint errors in utils.js

--- utils.js
+++ utils.js
@@ -1,10 +1,10 @@
 export function didUserWin(userChoice, computerChoice){
-    if(userChoice === computerChoice){return 'tied'}
-    else if(userChoice === 'rock' && computerChoice === 'scissors'){return 'win'}
-    else if(userChoice === 'scissors' && computerChoice === 'paper'){return 'win'}
-    else if(userChoice === 'paper' && computerChoice === 'rock'){return 'win'}
-    else{return 'lose'}
-};
+    if (userChoice === computerChoice){return 'tied';}
+    else if (userChoice === 'rock' && computerChoice === 'scissors'){return 'win';}
+    else if (userChoice === 'scissors' && computerChoice === 'paper'){return 'win';}
+    else if (userChoice === 'paper' && computerChoice === 'rock'){return 'win';}
+    else {return 'lose';}
+}
 
 export function getRandomThrow(){
     return ['rock', 'paper', 'scissors'][Math.round(Math.random() * 2)];
